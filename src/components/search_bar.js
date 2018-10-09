53686a79d83f3b92fefe825ce1828ce7981e5a40
Liam Matthews <mattheli@me.com>
2018-10-09 21:07:31 +0100

    Search Bar

---
+++ src/components/search_bar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+/* functional component
+const SearchBar = () => {
+    return <input />;
+}
+*/
+
+class SearchBar extends Component {
+    render() {
+        return <input />;
+    }
+}
+
+export default SearchBar;
