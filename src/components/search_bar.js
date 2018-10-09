0db09e6b53b658596d843d5108a6deb58a8efa53
Liam Matthews <mattheli@me.com>
2018-10-09 21:42:01 +0100

    First stage search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,8 +7,21 @@ const SearchBar = () => {
 */
 
 class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
     render() {
-        return <input />;
+        return (
+            <div>
+                <input 
+                    value={this.state.term}
+                    onChange={ event => this.setState({ term: event.target.value }) } />
+            </div>
+        );
     }
 }
 
